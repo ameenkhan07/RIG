f46cbc296e1ccb7fa34ec35e816f076980acde39
Avishkar Gupta <rootavish@gmail.com>
2015-04-19 10:31:12 +0530

    SQL tables

---
+++ table.sql
@@ -0,0 +1,13 @@
+ CREATE TABLE complaints(
+    district varchar(20),
+    block varchar(20),
+    name varchar(25),
+    address varchar(65),
+    complaint varchar(400),
+    product varchar(40),
+    email varchar(25),
+    img varchar(30));
+
+CREATE TABLE company(
+    username varchar(20),
+    password varchar(24));
